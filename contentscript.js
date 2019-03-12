fe32b8ef3a7f9fe0d6b959e207b0f77baf430ba3
Lukas <lukas.jordan@me.com>
2019-03-12 22:59:21 +0100

    Added Support for Trailer, improved Performance and Maintainablity

--- contentscript.js
+++ contentscript.js
@@ -4,13 +4,14 @@ const JustWatch = require('justwatch-api');
 background
 */
 
-var allProviders = [];
 var jw = new JustWatch({locale: getLocale()});
 var jweng = new JustWatch({locale: 'en_US'});
+var allProviders;
+var trailerProvider = "https://www.invidio.us/watch?v=";
 
 //get user location
 function getLocale() {
-  return  navigator.language.replace("-", "_");
+  return  'de_DE';//navigator.language.replace("-", "_");
 }
 
 //parse film title from letterboxd page
@@ -18,23 +19,39 @@ function getFilmTitle() {
   return $("#featured-film-header").children("h1").text();
 }
 
+//parse film release year from letterboxd page
 function getFilmYear() {
   return $("#featured-film-header").children("p").children("small").children("a").text();
 }
 
 //get justwatch-id of the film
-async function getFilmID() {
+async function getFilmID(title, year) {
   var t0 = performance.now();
-  var result = await jweng.search({query: getFilmTitle(), cinema_release: getFilmYear()});
+  var result = await jweng.search({query: title, cinema_release: year});
   var t1 = performance.now();
-  console.log(result.items[0].title + ", " + result.items[0].original_release_year +  " zu finden hat " + (t1-t0) + "ms gedauert");
+  console.log("Streamboxd: " + result.items[0].title + ", " + result.items[0].original_release_year +  " zu finden hat " + Math.round(t1-t0) + "ms gedauert");
   return result.items[0].id
 }
 
 //get film information from justwatch
-async function getFilm() {
-  var film = await jw.getTitle('movie', await getFilmID());
+async function getFilm(id) {
+  var film = await jw.getTitle('movie', id);
   return film;
+}
+
+//get film trailer
+function getTrailer(film) {
+  if (film.clips != null) {
+    var trailerID = film.clips.filter(clip => clip.type == "trailer")[0].external_id;
+  } else {
+    var trailerID = $(".watch-panel").children("p").children("a").attr("href").substr(24,11);
+  }
+  return trailerProvider + trailerID;
+}
+
+//parse film trailer
+function getTrailerFromLetterboxd() {
+  return
 }
 
 //get all available providers from justwatch
@@ -44,8 +61,7 @@ async function getAllProviders() {
 }
 
 //get providers that offer the film from justwatch
-async function getFilmProviders() {
-  var film = await getFilm();
+function getFilmProviders(film) {
   return film.offers.filter(provider => provider.monetization_type == "flatrate" && provider.presentation_type == "hd")
 }
 
@@ -64,33 +80,40 @@ create html
 */
 
 //create a provider panel
-function createProviderPanel(provider, url) {
+function getProviderPanel(provider, url) {
+  var i = $("<span></span>").addClass("icon -play");
   var s = $("<span></span>").addClass("name").text(provider);
-  var a = $("<a></a>").attr("href", url).addClass("label").append(s);
+  var a = $("<a></a>").attr("href", url).addClass("label").append(i).append(s);
   return $("<p></p>").append(a);
 }
 
 //get all available provider panels
-async function getProviderPanels() {
+function createProviderPanels(providers) {
   var providerPanel = $("<section></section>").addClass("provider");
-  allProviders = await getAllProviders();
-  var providers = await getFilmProviders();
   for (var i = 0; i < providers.length; i++) {
-    $(providerPanel).append(createProviderPanel(IDtoProvider(providers[i].provider_id), providers[i].urls.standard_web));
+    $(providerPanel).append(getProviderPanel(IDtoProvider(providers[i].provider_id), providers[i].urls.standard_web));
   }
   return providerPanel;
 }
 
 //display panel
-async function createStreamPanel() {
-  var streamPanel = $("<section></section>").attr('id', 'stream-panel').addClass("watch-panel").css("margin-top", "20px");
-  var streamPanelTitle = $("<h3></h3>").addClass("title").text("Stream");
-  var providerPanel = await getProviderPanels();
-  $(".watch-panel").after(streamPanel.append(streamPanelTitle).append(providerPanel));
+function createStreamPanel(trailer, provider) {
+  var streamPanel = $("<section></section>").attr('id', 'stream-panel').addClass("watch-panel");
+  var title = $("<h3></h3>").addClass("title").text("Watch");
+  return streamPanel.append(title).append(trailer).append(provider);
 }
 
 /*
 main
 */
 
-createStreamPanel();
+async function main() {
+  var film = await getFilm(await getFilmID(getFilmTitle(), getFilmYear()));
+  allProviders = await getAllProviders();
+  var trailer = getProviderPanel("Trailer", getTrailer(film))
+  var streamProviders = createProviderPanels(getFilmProviders(film));
+  $(".watch-panel").remove();
+  $(".poster-list").after(createStreamPanel(trailer, streamProviders));
+}
+
+main();
