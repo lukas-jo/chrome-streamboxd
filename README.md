ebe9e2573f3db03f7dab3f2e4c42b72212050be0
Lukas <lukas.jordan@me.com>
2019-03-31 22:33:03 +0200

    Stable Release

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+﻿
 # chrome-streamboxd
+
 This chrome extension shows on a Letterboxd film page where this film can be streamed
+
+## About
+
+While Letterboxd already shows where a film can be streamed, this only counts for the USA. This extension allows everyone to see where a film can be streamed in their respective country.
+
+![](https://image.prntscr.com/image/zW7PKzOlQ8et-FXENm3JJA.png =450x)
+
+## Install
+
+ 1. Clone this repository or download the zip & unpack it
+ 2. go to [chrome://extensions](chrome://extensions) and enable Developer Mode
+ 3. Click 'Load Unpacked' and select the chrome-streamboxd folder
+ 4. Done
+
+
+## Test
+
+This Extension uses the node package [justwatch-api](https://github.com/lufinkey/node-justwatch-api) which is a wrapper for the API of justwatch.com. [Browserify](http://browserify.org/) was used to allow the node package to be used in the browser.
--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,11 @@
 const JustWatch = require('justwatch-api');
 
 /*
-background
+globals
 */
 
 var jw;
-var jweng = new JustWatch({
-  locale: 'en_US'
-});
+var jweng = new JustWatch({locale: 'en_US'});
 var allProviders;
 var trailerProvider = "https://www.invidio.us/watch?v=";
 
@@ -15,49 +13,84 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
   main();
 });
 
-//get user location
-function setLocale() {
-  chrome.storage.local.get(['options'], function(result) {
-    var pattloc = /[a-z][a-z]_[A-Z][A-Z]/;
-    var loc = navigator.language.replace("-", "_");
-    var opt = JSON.parse(result.options);
-    var resloc = opt.locale;
-    if (resloc != null && pattloc.test(resloc)) {
-      loc = resloc;
-    }
-    jw = new JustWatch({
-      locale: loc
-    });
-  });
-}
+/*
+parse html
+*/
 
 //parse film title from letterboxd page
 function getFilmTitle() {
-  try {
     return $("#featured-film-header").children("h1").text();
-  } catch (err) {
-    console.log("No Film Title");
-    return null;
-  }
 }
 
 //parse tMDB ID from letterboxd page
-function getTmdbId() {
+function getLBTmdbId() {
   return parseInt($("body").attr("data-tmdb-id"));
 }
+
+//parse Traile ID from letterboxd page
+function getLBTrailerId() {
+  return $(".watch-panel").children("p").children("a").attr("href").substr(24, 11);
+}
+
+/*
+background & helper
+*/
+
+//check if a String is a locale
+function isLocale(locale) {
+  return typeof locale === "string" && /[a-z][a-z]_[A-Z][A-Z]/.test(locale)
+}
+
+//get user location
+function setLocale() {
+  chrome.storage.local.get(['options'], function(result) {
+    var opt = JSON.parse(result.options);
+    if (isLocale(opt.locale)) {
+      var loc = opt.locale;
+    } else  {
+      var loc =navigator.language.replace("-", "_");
+    }
+    jw = new JustWatch({locale: loc});
+  });
+}
+
+//find the tMDB ID of a film from justwatch
+function getJWTmdbId(scoring = []) {
+  return scoring.find(item => item.provider_type == "tmdb:id").value;
+}
+
+//find a film in a list of film from justwatch
+function findFilm(films = []) {
+  return films.find(film => getJWTmdbId(film.scoring) == getLBTmdbId());
+}
+
+//get the Trailer ID from justwatch
+function getJWTrailerId(clips = []) {
+  return clips.filter(clip => clip.type == "trailer")[0].external_id;
+}
+
+//returns only uniqie Providers: eg. Netflix is listed twice as HD and SD
+function uniqueProviders(providers = []) {
+	return providers.filter( (v, i, a) => a.map(v => v.provider_id).indexOf(v.provider_id) === i );
+}
+
+//turn provider id from justwatch to provider name
+function IDtoProvider(id = 0) {
+  return allProviders.find(
+    provider => provider.id == id
+  ).clear_name;
+}
+
+/*
+justwatch
+*/
 
 //get justwatch-id of the film
 async function getFilmID(title) {
   try {
-    var result = await jweng.search({
-      query: title
-    });
-    return result.items.find(function(film) {
-      return film.scoring.find(function(item) {
-        return item.provider_type == "tmdb:id";
-      }).value == getTmdbId()
-    }).id;
-  } catch {
+    var result = await jweng.search({query: title});
+    return findFilm(result.items).id;
+  } catch(err) {
     console.log("Film not found");
     return null;
   }
@@ -68,6 +101,7 @@ async function getFilm(id) {
   if (id != null) {
     return await jw.getTitle('movie', id);
   } else {
+    console.log("no film");
     return null;
   }
 }
@@ -75,39 +109,24 @@ async function getFilm(id) {
 //get film trailer from justwatch-api or parse it from letterboxd
 function getTrailer(film) {
   if (film != null && film.clips != null) {
-    var trailerID = film.clips.filter(clip => clip.type == "trailer")[0].external_id;
-  } else if ($(".watch-panel").children("p").children("a").attr("href") != null) {
-    var trailerID = $(".watch-panel").children("p").children("a").attr("href").substr(24, 11);
-  } else {
+    return trailerProvider + getJWTrailerId(film.clips);
+  } try {
+    return trailerProvider + getLBTrailerId()
+  } catch(err) {
+    console.log("no trailer");
     return null;
-  }
-  return trailerProvider + trailerID;
-}
-
-//get all available providers from justwatch
-async function getAllProviders() {
-  var providers = await jw.getProviders();
-  if (providers != null) {
-    return providers.filter(provider => provider.monetization_types.includes("flatrate"));
-  } else {
-    return [];
   }
 }
 
 //get providers that offer the film from justwatch
 function getFilmProviders(film) {
   if (film != null && film.offers != null) {
-    return film.offers.filter(provider => provider.monetization_type == "flatrate");
+    return film.offers.filter(
+      provider => provider.monetization_type == "flatrate"
+    );
   } else {
     return [];
   }
-}
-
-//turn provider id from justwatch to provider name
-function IDtoProvider(id) {
-  return allProviders.find(function(provider) {
-    return provider.id == id;
-  }).clear_name;
 }
 
 /*
@@ -125,21 +144,9 @@ function getProviderPanel(provider, url) {
 //get all available provider panels
 function createProviderPanels(trailer, providers) {
   var providerPanel = $("<section></section>").addClass("provider");
-  if (trailer != null || providers.length > 0) {
     trailer != null && $(providerPanel).append(getProviderPanel("Trailer", trailer));
-    var ind = [];
-    var id = 0;
-    for (var i = 0; i < providers.length; i++) {
-      id = providers[i].provider_id;
-      ind.push(id);
-      if (ind.indexOf(id) == i) {
-        $(providerPanel).append(getProviderPanel(IDtoProvider(id), providers[i].urls.standard_web));
-      }
-    }
+    uniqueProviders(providers).forEach(p => $(providerPanel).append(getProviderPanel(IDtoProvider(p.provider_id), p.urls.standard_web)));
     return providerPanel;
-  } else {
-    throw "nothing found";
-  }
 }
 
 //display panel
@@ -156,7 +163,7 @@ main
 async function main() {
   setLocale();
   var film = await getFilm(await getFilmID(getFilmTitle()));
-  allProviders = await getAllProviders();
+  allProviders = await jw.getProviders();
   var streamProviders = createProviderPanels(getTrailer(film), getFilmProviders(film));
   var streamPanel = createStreamPanel(streamProviders);
   $(".watch-panel").replaceWith(streamPanel);
