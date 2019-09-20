3fdf62c02aca2649a22e58951bdede3da91eb235
Lukas <lukas.jordan@me.com>
2019-09-20 23:28:04 +0200

    Removed justwatch-api dependency

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,4 @@
-const JustWatch = require('justwatch-api');
-const engine = new JustWatch({locale: 'en_US'});
-let jw, allProviders;
+let allProviders;
 
 /*
 parse html
@@ -8,17 +6,17 @@ parse html
 
 //parse film title from letterboxd page
 function getFilmTitle() {
-  return $("#featured-film-header").children("h1").text();
+  return document.querySelector("#featured-film-header").firstElementChild.innerText;
 }
 
 //parse tMDB ID from letterboxd page
 function getLBTmdbId() {
-  return parseInt($("body").attr("data-tmdb-id"));
+  return parseInt(document.querySelector("body").dataset.tmdbId);
 }
 
 //parse Traile ID from letterboxd page
 function getLBTrailerId() {
-  return $(".watch-panel").children("p").children("a").attr("href").substr(24, 11);
+  return document.querySelector(".watch-panel").children[1].firstElementChild.href.substr(24, 11);
 }
 
 /*
@@ -82,29 +80,61 @@ function IDtoProvider(id = 0) {
 justwatch
 */
 
+function jsonToQuery(data) {
+  return Object.entries(data).map(a => a[0] + '=' + a[1]).join('&');
+}
+
+function request(method, endpoint, data) {
+  return new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    let body = null, queryString = '';
+    if (method === 'GET' && data != null && data.constructor === Object && Object.entries(data).length === 0) {
+      queryString = '?' + jsonToQuery(data)
+    } else {
+      body = JSON.stringify(data);
+    }
+    xhr.open(method, 'https://cors-anywhere.herokuapp.com/apis.justwatch.com/content' + endpoint + queryString);
+    xhr.onload = function() {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function() {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send(body);
+  });
+}
+
+async function getProviders() {
+  const res = await request("GET", `/providers/locale/${getLocale()}`, null);
+  return JSON.parse(res);
+}
+
 //search on justwatch for a film
-async function getFilmOV(title) {
-  try {
-    let result = await engine.search({query: title});
-    return findFilm(result.items);
-  } catch (err) {
-    console.log("Could not find film with title " + title);
-    return {};
-  }
+async function searchTitle(title) {
+  const res = await request("POST", '/titles/en_US/popular', {
+    query: title
+  });
+  return findFilm(JSON.parse(res).items);
 }
 
 //get film information from justwatch
 async function getFilm(id = 0) {
-  try {
-    return await jw.getTitle('movie', id);
-  } catch (err) {
-    console.log(id + " is not a valid ID")
-    return {};
-  }
+  const res = await request("GET", `/titles/movie/${id}/locale/${getLocale()}`, null);
+  return JSON.parse(res);
 }
 
 //get film trailer from justwatch-api or parse it from letterboxd
-function getTrailer({clips}) {
+function getTrailer({ clips }) {
   if (clips != null) {
     return options.trailerProvider + getJWTrailerId(clips);
   }
@@ -117,7 +147,7 @@ function getTrailer({clips}) {
 }
 
 //get providers that offer the film from justwatch
-function getFilmProviders({offers}) {
+function getFilmProviders({ offers }) {
   if (offers != null) {
     return offers.filter(
       provider => provider.monetization_type === "flatrate"
@@ -158,9 +188,8 @@ main
 async function main() {
   setStorageListener();
   options = await getOptions();
-  jw = new JustWatch({locale: getLocale()});
-  allProviders = await jw.getProviders();
-  let filmOV = await getFilmOV(getFilmTitle());
+  allProviders = await getProviders();
+  let filmOV = await searchTitle(getFilmTitle());
   let film = await getFilm(filmOV.id);
   let trailer = getTrailer(options.trailerOV ? filmOV : film);
   let streamPanel = createStreamPanel(trailer, getFilmProviders(film));
